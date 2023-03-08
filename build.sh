cb19c20eabae7de5c5126ebc99b1daadcacc1f05
Devopskiller <105400270+rohithsgr@users.noreply.github.com>
2023-03-09 00:48:33 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ cat Dockerfile
 
 echo "Now we will create an image using this docker file with build number"
 
-docker build -t rohithbuildapp:${BUILD_NUMBER}-1 .
+docker build -t rohithbuildapp:${BUILD_NUMBER-1} .
 
 
 
